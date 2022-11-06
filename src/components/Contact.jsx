2cf833af9b6b5b9924beb45edaeed92dcd8416a6
zykscode <111462249+zykscode@users.noreply.github.com>
2022-11-06 10:32:21 +0100

    fix:stage3 baby final commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,53 +1,62 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import ReactTooltip from 'react-tooltip';
+import React, { useState, useRef } from "react";
+import ReactTooltip from "react-tooltip";
+import Share from "./Share";
+import useCopyToClipboard from "./useCopyToClipboard";
+import { Link } from "react-router-dom";
 
-import Share from './Share';
-import useCopyToClipboard from './useCopyToClipboard';
+const Links = (props) => {
+  const [anim, setAnim] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const ref = useRef(null);
 
-
-const Contacts = (props) => {
-  const [anim, setAnim] = useState(true);
-
-  const handleAnim = (e) => {
-    console.log('prevent')
-    e.preventDefault()
+  const clickHandle = (event) => {
+    event.stopPropagation();
+    if (!clicked) {
+      event.preventDefault();
+      setClicked(() => !clicked);
+    } else {
+      setClicked(() => !clicked);
+      setTimeout(setAnim(!anim), 7000);
+    }
   };
 
-    const [ value, copy] = useCopyToClipboard();
+  const [value, copy] = useCopyToClipboard();
+
   return (
     <>
-    
       <Link
         to='/contact'
         id={props.id}
         href={props.link}
         title={props.subtext}
+        onMouseEnter={() => setAnim(true)}
+        onMouseLeave={() => setAnim(false)}
         data-tip={props.subtext}
         className={` overflow-clip rounded-lg flex justify-center h-[76px] ${
-          anim ? 'focus:bg-[#FCFCFD]' : 'focus:bg-[#98A2B3] '
+          anim ? "focus:bg-[#FCFCFD]" : "focus:bg-[#98A2B3] "
         } bg-[#EAECF0]  hover:bg-[#D0D5DD]  `}
-        rel="noreferrer"
-      >
-        {' '}
+        rel='noreferrer'>
+        {" "}
         {
-          <div className="flex items-center h-full overflow-clip w-full gap-3 justify-center">
+          <div
+            className={`flex items-center h-full overflow-clip w-full gap-3 justify-center active:transform active:transition active:duration-500 active-translate-x-4 ${
+              anim && "transform transition duration-500 -translate-x-4"
+            }`}>
             <div
-              
-              className={`flex justify-end   h-full  bg-green-300 gap-1 `}
-            >
-              <span className={`text-[#101828] flex justify-center bg-gray-400 `}>
-                <span className="text2 font-bold"> {props.name}</span>
-              </span>
+              className={`flex justify-center  text-[#101828]  text2 font-bold  items-center  h-full  gap-1 `}>
+              {props.name}
             </div>
             <div
-              
-              className="justify-self-start   f-full"
-            > 
+              id='share'
+              ref={ref}
+              onClick={(e) => {
+                clickHandle(e);
+                setClicked(() => true);
+              }}
+              className={`'justify-self-start' ${anim ? "block" : "hidden"}`}>
               <Share
-                onClick={(e)=>{
-                  console.log(e)
-                  copy(props.link)
+                onClick={() => {
+                  copy(props.link);
                 }}
               />
             </div>
@@ -55,13 +64,13 @@ const Contacts = (props) => {
         }
       </Link>
       <ReactTooltip
-        place="bottom"
+        place='bottom'
         multiline={true}
         clickable={true}
-        effect="solid"
+        effect='solid'
       />
     </>
   );
 };
 
-export default Contacts;
+export default Links;
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,57 +1,65 @@
-import React, { useState } from 'react';
-import ReactTooltip from 'react-tooltip';
-
-import Share from './Share';
-import useCopyToClipboard from './useCopyToClipboard';
-import { motion } from 'framer-motion';
-
-const variants = {
-  open: { opacity: 1, scale: 1.5 },
-  closed: { opacity: 0 },
-};
+import React, { useState, useRef } from "react";
+import ReactTooltip from "react-tooltip";
+import Share from "./Share";
+import useCopyToClipboard from "./useCopyToClipboard";
 
 const Links = (props) => {
-  const [anim, setAnim] = useState(true);
+  const [anim, setAnim] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const ref = useRef(null);
 
-  const handleAnim = (e) => {
-    console.log('prevent')
-    e.preventDefault()
+  const clickHandle = (event) => {
+    event.stopPropagation();
+    if (!clicked) {
+      event.preventDefault();
+      setClicked(() => !clicked);
+    }else{
+      
+      setClicked(() => !clicked)
+      setTimeout(setAnim(!anim),7000)
+    }
   };
 
-    const [ value, copy] = useCopyToClipboard();
+  
+
+
+  const [value, copy] = useCopyToClipboard();
+
   return (
     <>
       <a
         id={props.id}
         href={props.link}
         title={props.subtext}
-        target="_blank"
-        onClick={handleAnim}
+        target='_blank'
+        onMouseEnter={() => setAnim(true)}
+        onMouseLeave={() => setAnim(false)}
         data-tip={props.subtext}
         className={` overflow-clip rounded-lg flex justify-center h-[76px] ${
-          anim ? 'focus:bg-[#FCFCFD]' : 'focus:bg-[#98A2B3] '
+          anim ? "focus:bg-[#FCFCFD]" : "focus:bg-[#98A2B3] "
         } bg-[#EAECF0]  hover:bg-[#D0D5DD]  `}
-        rel="noreferrer"
-      >
-        {' '}
+        rel='noreferrer'>
+        {" "}
         {
-          <div className="flex items-center h-full overflow-clip w-full gap-3 justify-center">
+          <div
+            className={`flex items-center h-full overflow-clip w-full gap-3 justify-center active:transform active:transition active:duration-500 active-translate-x-4 ${
+              anim && "transform transition duration-500 -translate-x-4"
+            }`}>
             <div
-              
-              className={`flex justify-end   h-full  gap-1 `}
-            >
-              <span className={`text-[#101828] flex justify-center `}>
-                <span className="text2 font-bold"> {props.name}</span>
-              </span>
+              className={`flex justify-center  text-[#101828]  text2 font-bold  items-center  h-full  gap-1 `}>
+              {props.name}
             </div>
             <div
-              
-              className="justify-self-start   f-full"
-            > 
+              id='share'
+              ref={ref}
+              onClick={(e) => {
+                clickHandle(e);
+                setClicked(() => true);
+              }}
+              className={`'justify-self-start' ${anim ? "block" : "hidden"}`}>
               <Share
-                onClick={(e)=>{
-                  console.log(e)
-                  copy(props.link)
+                onClick={() => {
+                  copy(props.link);
                 }}
               />
             </div>
@@ -59,10 +67,10 @@ const Links = (props) => {
         }
       </a>
       <ReactTooltip
-        place="bottom"
+        place='bottom'
         multiline={true}
         clickable={true}
-        effect="solid"
+        effect='solid'
       />
     </>
   );
